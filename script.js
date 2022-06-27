7c39499caefb2bcd76e220425a5c0bb2ebf9a59c
davron1104 <75130018+davron1104@users.noreply.github.com>
2022-06-27 15:10:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,38 @@
 // Место для первой задачи
 
 // showExperience(personalPlanPeter) => '1 month';
-    const personalPlanPeter = {
-        name: "Peter",
-        age: "29",
-        skills: {
-            languages: ['ru', 'eng'],
-            programmingLangs: {
-                js: '20%',
-                php: '10%'
-            },
-            exp: '1 month'
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
         },
-    
-    };
+        exp: '1 month'
+    },
 
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками: `
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+        return str;
+    }
+};
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
 
 function showProgrammingLangs(plan) {
-    let str = '';
-    const {programmingLangs} = plan.skills; 
-    for (let key in programmingLangs) {
-        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
-    }
-    return str;
+let str = '';
+const {programmingLangs} = plan.skills; 
+for (let key in programmingLangs) {
+    str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+}
+return str;
 }
 showProgrammingLangs(personalPlanPeter);
 
